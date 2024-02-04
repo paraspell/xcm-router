f7b0c310f63d8194683fed0b327602d7f2fa58ce
Rikkard29 <absolon.mm@gmail.com>
2024-02-04 14:46:36 +0100

    fix: Fix vite production build

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,25 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   build: {
     target: 'esnext',
   },
   optimizeDeps: {
     esbuildOptions: {
       target: 'esnext',
-      define: {
-        global: 'globalThis',
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
     },
   },
 });
